6cabefeb922de54be500e26328237cd1fe407271
dfoley84 <83467582+dfoley84@users.noreply.github.com>
2022-01-13 02:10:04 -0800

    Add files via upload

---
+++ GameList/frontend/src/components/footer.vue
@@ -0,0 +1,4 @@
+<template>
+        <!-- Footer -->
+        <Footer class="bg-primary text-white text-center" style="border-radius:10px">Copyright &copy;. All Rights Reserved 2021.</Footer>
+</template>
---
+++ GameList/frontend/src/router/index.js
@@ -0,0 +1,22 @@
+import Vue from "vue";
+import VueRouter from "vue-router";
+import Games from '../components/Games.vue'
+
+
+Vue.use(VueRouter);
+
+const routes = [
+ {
+  path : '/games',
+  name : 'Games',
+  component : Games,
+}
+];
+
+const router = new VueRouter({
+  mode: "history",
+  base: process.env.BASE_URL,
+  routes,
+});
+
+export default router;
