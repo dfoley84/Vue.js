92f2934414ecd416154a69c2c6b76ce766e6eb6e
dfoley84 <83467582+dfoley84@users.noreply.github.com>
2022-01-16 01:22:26 +0000

    Update main.js

--- vCenter/frontend/main.js
+++ vCenter/frontend/main.js
@@ -2,17 +2,22 @@ import Vue from "vue";
 import App from "./App.vue";
 import router from "./router";
 
-import 'bootstrap/dist/css/bootstrap.css';
-import 'bootstrap-vue/dist/bootstrap-vue.css';
-import BootstrapVue from 'bootstrap-vue';
-import "@fortawesome/fontawesome-free/js/all"
+import "bootstrap/dist/css/bootstrap.css";
+import "bootstrap-vue/dist/bootstrap-vue.css";
+import BootstrapVue from "bootstrap-vue";
 
+import { library } from "@fortawesome/fontawesome-svg-core";
+import { fas } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/vue-fontawesome";
+import vuetify from "./plugins/vuetify";
 
 Vue.config.productionTip = false;
-Vue.use(BootstrapVue)
-
+Vue.use(BootstrapVue);
+library.add(fas);
+Vue.component("font-awesome-icon", FontAwesomeIcon);
 
 new Vue({
   router,
+  vuetify,
   render: (h) => h(App),
 }).$mount("#app");
