049a85d8692274429d3eccaff50f7b244d5a2cb8
dfoley84 <83467582+dfoley84@users.noreply.github.com>
2022-01-19 20:25:58 +0000

    Update app.py

--- vSphere/backend/app.py
+++ vSphere/backend/app.py
@@ -3,9 +3,11 @@ from flask_cors import CORS
 from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy import select, update, delete, values
 from dbModel import vCenter, Horizon
+import pika, json 
+
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://myuser://@/vSphere'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://myuser://@FLASK'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
 
@@ -22,31 +24,56 @@ app.config.from_object(__name__)
 CORS(app, resources={r'/*': {'origins': '*'}})
 
 
-#Get vDesks
-@app.route('/horizon', methods=['GET'])
+@app.route('/horizon', methods=['GET','POST'])
 def GetvDesks():
     response_object = {'status': 'success'}
-    response_object['vdesks'] = [i.serialize for i in Horizon.query.all()]
-    db.session.remove()
+    if request.method == 'POST':
+        #Getting Posted JSON Data 
+        message = request.get_json()
+
+        #Getting Machine Information from FontEnd.
+        PowerCycle = message['PowerCycle']
+        MachineName = message['vDesk']['MachineName']
+
+        #Pass Information to RabbitMQ For Jenkins Jobs
+        connection = pika.BlockingConnection(pika.ConnectionParameters(host='localhost'))
+        channel = connection.channel()
+        channel.queue_declare(queue='vDeskPowerCycle', durable=False)
+        channel.basic_publish(
+            exchange='',
+            routing_key='vDeskPowerCycle',
+            body=json.dumps(message),
+            properties=pika.BasicProperties(
+            delivery_mode=2,
+        ))
+        connection.close()
+
+        #Updating DB Based on PowerCycle
+        if PowerCycle == 'Delete':
+            RemovevDesks = delete(Horizon).where(Horizon.MachineName == MachineName)
+            db.session.execute(RemovevDesks)
+            db.session.commit()
+        else:
+            #Append Running Job to Machine Status.
+            print('')
+        
+    else:
+        response_object['vdesks'] = [i.serialize for i in Horizon.query.all()]
+        db.session.remove()
     return jsonify(response_object)
 
-  
-#Get Searched vDesks based on User
+#Get Searched User
 @app.route("/searchdata", methods=['GET', 'POST'])
 def searchdata():
     response_object = {'status':'success'}
     if request.method == 'POST':
         post_data = request.get_json()
         Username = post_data.get('TitleUser')
-        response_object['vdesks'] = [i.serialize for i in Horizon.query.filter_by(UserName=Username).all()]
+        print(Username)
+        response_object['SearchvDesks'] = [i.serialize for i in Horizon.query.filter_by(UserName=Username).all()]
         db.session.remove()
     return jsonify(response_object)
 
-#Get vCenter Servers
-
-#Get Searched vCenter Servers based on User
-
-#Get VMWare on AWS
 
 
 if __name__ == '__main__':
