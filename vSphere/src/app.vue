71ca2e127359b8171793e871a3cff27fb255af75
dfoley84 <83467582+dfoley84@users.noreply.github.com>
2022-01-19 03:42:20 -0800

    Update app.vue

--- vSphere/src/app.vue
+++ vSphere/src/app.vue
@@ -1,30 +1,27 @@
 <template>
-  <div id="nav">
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
+  <div id="app">
+    <Headerbar />
+    <div class="main-section columns">
+      <Navigation />
+      <main class="column">
+        <router-view />
+      </main>
+    </div>
   </div>
-  <router-view />
 </template>
 
+
+<script>
+import Headerbar from '@/components/Header/Header.vue'
+import Navigation from '@/components/NavigationBar/Navigation.vue'
+export default{
+   components: { 
+     Headerbar,
+     Navigation
+  },
+};
+</script>
+
 <style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
-
-#nav {
-  padding: 30px;
-
-  a {
-    font-weight: bold;
-    color: #2c3e50;
-
-    &.router-link-exact-active {
-      color: #42b983;
-    }
-  }
-}
+@import '@/design/index.scss';
 </style>
