dab8ef04de543463cc8769f9aeb845511fb26e9a
dfoley84 <83467582+dfoley84@users.noreply.github.com>
2022-01-19 12:07:23 +0000

    Create Footer.vue

---
+++ vSphere/src/components/Footer/Footer.vue
@@ -0,0 +1,9 @@
+<template>
+<div class="container">
+    <div class="row">
+        <footer style="text-align: center;" >
+            ©{{ new Date().getFullYear() }}
+        </footer>
+    </div>
+</div>
+</template>
