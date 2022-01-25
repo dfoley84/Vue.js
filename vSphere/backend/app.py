105446b097ed36a236eef4f7b560e5085f5b5a21
dfoley84 <83467582+dfoley84@users.noreply.github.com>
2022-01-25 01:13:36 -0800

    Update app.py

--- vSphere/backend/app.py
+++ vSphere/backend/app.py
@@ -1,3 +1,4 @@
+from re import M
 from flask import Flask, jsonify, request 
 from flask_cors import CORS
 from flask_sqlalchemy import SQLAlchemy
@@ -7,7 +8,7 @@ from rabbitmq import RabbitMQ_Sender
 
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql:'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://myuser:'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
 
@@ -17,40 +18,32 @@ SQLALCHEMY_ENGINE_OPTIONS = {
     "pool_recycle": 160 * 160,
     "pool_size": 1000,
 }
-
 app.config.from_object(__name__)
 
 # enable CORS
 CORS(app, resources={r'/*': {'origins': '*'}})
 
 
+#SQL Alchemy Functions
+def DeleteMachine(MachineName):
+    RemovevDesks = delete(Horizon).where(Horizon.MachineName == MachineName)
+    db.session.execute(RemovevDesks)
+    db.session.commit()
+    db.session.remove()
+
+def UpdateMachine(MachineName):
+    db.session.query(Horizon).filter(Horizon.MachineName == MachineName).update({"MachineStatus": "Running Job"})
+    db.session.commit()
+    db.session.remove()
+
+
+
 #Getting All vDesks
-@app.route('/horizon', methods=['GET','POST'])
+@app.route('/horizon', methods=['GET'])
 def GetvDesks():
     response_object = {'status': 'success'}
-    if request.method == 'POST':
-        message = request.get_json() #Getting Posted JSON Data 
-
-        #Getting Machine Information from FontEnd.
-        PowerCycle = message['PowerCycle']
-        MachineName = message['vDesk']['MachineName']
-        
-        RabbitMQ_Sender(message) #Passing Message to Class RabbitMQ_Sender
-        
-        #Updating DB Based on PowerCycle
-        if PowerCycle == 'Delete':
-            RemovevDesks = delete(Horizon).where(Horizon.MachineName == MachineName)
-            db.session.execute(RemovevDesks)
-            db.session.commit()
-            db.session.remove()
-        else:
-            print(MachineName)
-            db.session.query(Horizon).filter(Horizon.MachineName == MachineName).update({"MachineStatus": "Running Job"})
-            db.session.commit()
-            db.session.remove()
-    else:
-        response_object['vdesks'] = [i.serialize for i in Horizon.query.all()]
-        db.session.remove()
+    response_object['vdesks'] = [i.serialize for i in Horizon.query.all()]
+    db.session.remove()
     return jsonify(response_object)
 
 
@@ -62,11 +55,47 @@ def searchdata():
     if request.method == 'POST':
         post_data = request.get_json()
         Username = post_data.get('TitleUser')
-        print(Username)
         response_object['SearchvDesks'] = [i.serialize for i in Horizon.query.filter_by(UserName=Username).all()]
         db.session.remove()
     return jsonify(response_object)
 
+
+#Display vDesks from Searched User
+@app.route("/powercycle", methods=['GET','POST'])
+def selectedMachine():
+    response_object = {'status':'success'}
+    if request.method == 'POST':
+        message = request.get_json() #Getting Posted JSON Data 
+
+        #Getting Machine Information from FontEnd.
+        PowerCycle = message['data']['PowerCycle']
+        MachineName = message['data']['vDesk']['MachineName']
+
+        RabbitMQ_Sender(message) #Passing Message to Class RabbitMQ_Sender
+        
+        #Updating DB Based on PowerCycle
+        if PowerCycle == 'Delete':
+           DeleteMachine(MachineName)
+        else:
+            UpdateMachine(MachineName)
+    return jsonify()
+
+
+@app.route("/searchdata/powercycle", methods=['GET','POST'])
+def PowerCycle():
+    response_object = {'status':'success'}
+    if request.method == 'POST':
+        message = request.get_json() #Getting Posted JSON Data 
+        #Getting Machine Information from FontEnd.
+        PowerCycle = message['PowerCycle']
+        MachineName = message['vDesk']['MachineName']
+        #RabbitMQ_Sender(message) #Passing Message to Class RabbitMQ_Sender
+        #Updating DB Based on PowerCycle
+        if PowerCycle == 'Delete':
+           DeleteMachine(MachineName)
+        else:
+            UpdateMachine(MachineName)
+    return jsonify()
 
 
 if __name__ == '__main__':
