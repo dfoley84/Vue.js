af0a1fa897d9f4e6e7dd89b0215fd5e1362b20cc
dfoley84 <83467582+dfoley84@users.noreply.github.com>
2022-01-25 01:15:31 -0800

    Update index.js

--- vSphere/src/store/index.js
+++ vSphere/src/store/index.js
@@ -5,23 +5,36 @@ import axios from "axios"
 export default createStore({
   state: {
     vdesks: [],
+    SearchvDesks: [],
     PostTitle: '',
   },
   
   getter: {
     TitleUser: state => {
       return state.PostTitle;
+    },
+    vdesk: state => {
+      return state.PostTitlevDesk;
+    },
+    TitleDesk: state => {
+      return state.Click;
     }
   },
   
   mutations: {
-    SET_VDESKS(state, payload){
-      state.vdesks = payload;
+  SET_VDESKS(state, payload){
+    state.vdesks = payload;
   },
-  PostTitle(state, newtitle) {
-    state.PostTitle = newtitle;
- }
+
+  SET_USERVDESKS(state, payload){
+    state.SearchvDesks = payload;
 },
+
+  PostTitle(state, payload) {
+    state.PostTitle = payload;
+ },
+},
+
   actions: {
     FeatchvDesks({commit}){
       const path = 'http://localhost:5000/horizon';
@@ -33,20 +46,33 @@ export default createStore({
           console.error(error)
       });
   },
-  
+
   FetchUservDesks({commit, state}) {
     const path = 'http://localhost:5000/searchdata';
     axios.post(path, {
       TitleUser: state.PostTitle
   })
     .then((res) => {
-        (commit("SET_VDESKS", res.data.vdesks))  
+        (commit("SET_USERVDESKS", res.data.SearchvDesks))  
     })
     .catch((error) => {
         console.error(error);
     });
   },
+
+ async postData({commit, state}, data) {
+  const path = 'http://localhost:5000/powercycle';
+  const result = await axios.post(path,
+    { TitleUser: state.PostTitle, data });
+    axios.get('http://localhost:5000/horizon')
+    .then((res) => {
+        (commit("SET_VDESKS", res.data.vdesks))
+    })
+    .catch((error) => {
+        console.error(error)
+    });
+  return result
+ }
 },
-  
   modules: {},
 });
